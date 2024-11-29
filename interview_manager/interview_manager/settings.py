2d2be79170f809e2aa88d6afe24ec6d9a20a2d6f
Harsh Verma <verma1a@uwindsor.ca>
2024-11-29 09:24:50 -0500

    Added URL for deployment on render

--- interview_manager/interview_manager/settings.py
+++ interview_manager/interview_manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)zg_j07m%_clt+e-6ibbx_=pk*xy_3i2!luah6xm*!r1421zkl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['interview-backend-0zsr.onrender.com']
 
 
 # Application definition
